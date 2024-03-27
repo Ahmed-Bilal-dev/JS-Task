a5f94fc9d7b036a91e1618652e2ec717263aa4d6
AHMED BILAL <162837323+Ahmed-Bilal-dev@users.noreply.github.com>
2024-03-27 14:31:09 -0700

    Add files via upload

---
+++ tasbeeh counter/app.js
@@ -0,0 +1,45 @@
+
+// card1 function
+let count1 = 0;
+const display1 = document.getElementById("display");
+
+function incrementCount1() {
+    count1++;
+    display1.textContent = count1;
+}
+
+function resetCount1() {
+    count1 = 0;
+    display1.textContent = count1;
+}
+
+
+// card2 function
+let count2 = 0;
+const display2 = document.getElementById("display2");
+
+function incrementCount2() {
+    count2++;
+    display2.textContent = count2;
+}
+
+function resetCount2() {
+    count2 = 0;
+    display2.textContent = count2;
+}
+
+
+// card3 function
+let count3 = 0;
+const display3 = document.getElementById("display3");
+
+function incrementCount3() {
+    count3++;
+    display3.textContent = count3;
+}
+
+function resetCount3() {
+    count3 = 0;
+    display3.textContent = count3;
+}
+       
